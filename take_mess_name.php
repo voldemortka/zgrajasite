134bfd8982725add29309bd461fc662b2a84fb19
voldemortka <samurajka37@gmail.com>
2024-09-08 21:03:50 +0200

    Update take_mess_name.php

--- take_mess_name.php
+++ take_mess_name.php
@@ -4,7 +4,7 @@
 
 session_start();
 require_once("connect.php");
-$connection = mysqli_connect($host, $db_user, $db_password, $db_name);
+$connection = pg_connect("host=$host dbname=$db_name user=$db_user password=$db_password port=$port");
 
 $data = json_decode(file_get_contents('php://input'), true);
 
@@ -14,15 +14,15 @@ $mess_id = $data['id'];
 $name = $_SESSION['username'];
 if(!isset($_SESSION['id'])){
     $sql1 = "select id from konto where name='".$name."'";
-    $res = mysqli_query($connection, $sql1);
-    $row = mysqli_fetch_row($res);
+    $res = pg_query($connection, $sql1);
+    $row = pg_fetch_row($res);
     $id = $row[0];
 } else $id = $_SESSION['id'];
 
 
 $dane = [
-    //'iss' => 'http://zgrajasite.kesug.com',
-    'iss' => "http://localhost/mine/zgraja%20site",
+    'iss' => 'https://zgrajasite.onrender.com',
+    //'iss' => "http://localhost/mine/zgraja%20site",
     'iat' => time(),
     'exp' => time() + 3600, // Token ważny przez 1 godzinę
     'data' => [
@@ -41,5 +41,5 @@ $jwt1 = JWT::encode($dane, "zgrajasite_mess", "HS256");
 localStorage.setItem('mess_token', '<?= $jwt1 ?>');
 
 // Przekierowanie do strony z grą
-window.location.href = 'http://localhost/mine/zgrajasite%20node/messenger.html';
+//window.location.href = 'http://localhost/mine/zgrajasite%20node/messenger.html';
 </script>
