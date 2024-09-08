55fdc4457a755a0f64f1577ac517273eb0c64ae1
voldemortka <samurajka37@gmail.com>
2024-09-08 21:41:36 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,11 @@
 <?php
-    $host="localhost";
+   /* $host="localhost";
     $db_user="root";
     $db_password="";
-    $db_name="zgraja";
+    $db_name="zgraja"; */
+
+    $host="aws-0-eu-central-1.pooler.supabase.com";
+    $db_user="postgres.hikyvsglbjlwjdnqtlgj";
+    $db_password="SlytherV35!";
+    $db_name="postgres";
+    $port = 6543;
