fc88eb7bcd6d76374851b9870ccae534f4f1c0d2
voldemortka <samurajka37@gmail.com>
2024-09-08 21:02:16 +0200

    Update trans_pacy.php

--- trans_pacy.php
+++ trans_pacy.php
@@ -4,35 +4,35 @@
 
     session_start();
     require_once("connect.php");
-    $connection = mysqli_connect($host, $db_user, $db_password, $db_name);
+    $connection = pg_connect("host=$host dbname=$db_name user=$db_user password=$db_password port=$port");
 
     $name = $_SESSION['username'];
     if(!isset($_SESSION['id'])){
-        $sql1 = "select id from konto where name=".$name;
-        $res = mysqli_query($connection, $sql1);
-        $row = mysqli_fetch_row($res);
+        $sql1 = "select id from konto where name='$name'";
+        $res = pg_query($connection, $sql1);
+        $row = pg_fetch_row($res);
         $id = $row[0];
     } else $id = $_SESSION['id'];
 
     $sql2 = "select id from aktualne where gra=7";
     $sql3 = "select id from ruch where gra=7 and action=4";
-    $res2 = mysqli_query($connection, $sql2);
-    $res3 = mysqli_query($connection, $sql3);
-    if($res3 -> num_rows >0 || $res2 -> num_rows>5) header('Location: transform.php');
+    $res2 = pg_query($connection, $sql2);
+    $res3 = pg_query($connection, $sql3);
+    if(pg_num_rows($res3) >0 || pg_num_rows($res2)>5) header('Location: transform.php');
     
     //tu już na 100% wchodzimy do gry
-    $nr = $res2 -> num_rows;
+    $nr = pg_num_rows($res2);
     $sql6 = "select id from aktualne where gra=7 and kto=".$id;
-    $res6 = mysqli_query($connection, $sql6);
-    if($res6 -> num_rows ==0){
+    $res6 = pg_query($connection, $sql6);
+    if(pg_num_rows($res6) ==0){
         $sql4 = "insert into aktualne(id, gra, kto, alive, in_game, pkt, linie) values (NULL, 7, ".$id.", 1, 0, 0, 0)";
-        mysqli_query($connection, $sql4);
+        pg_query($connection, $sql4);
     }
 
     $tab = [];
     $sql7 = "select konto.name as name from konto inner join aktualne on konto.id=aktualne.kto where aktualne.gra=7 order by aktualne.id";
-    $res = mysqli_query($connection, $sql7);
-    while($row = $res -> fetch_assoc()){
+    $res = pg_query($connection, $sql7);
+    while($row = pg_fetch_assoc($res)){
         $tab[] = $row['name'];
     }
 
@@ -69,5 +69,5 @@
     localStorage.setItem('pacy2_token', '<?= $jwt2 ?>');
     
     // Przekierowanie do strony z grą
-    window.location.href = 'http://localhost/mine/zgrajasite%20node/pacman2/index.html';
+    window.location.href = 'https://zgrajasitenode.onrender.com/pacman2/';
     </script>
