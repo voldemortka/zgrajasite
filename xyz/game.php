18de0715a6fa9d640581217a77354763d49e51c2
voldemortka <samurajka37@gmail.com>
2024-11-12 21:44:26 +0100

    Update game.php

--- xyz/game.php
+++ xyz/game.php
@@ -30,7 +30,8 @@
     </head>
     <body onload='start(<?php echo json_encode(["passes" => $passes, "kats" => $kats]); ?>)'>
         <div id='go_back'><a href='index.html'>Ustaw nowe hasło</a></div>
-        <div id='go_back' onclick='next()'>POKAŻ HASŁO</div>
+        <div id='go_back' onclick='next()'>Przejdź do kolejnego hasła</div>
+        <div id='go_back' onclick='poka()'>Pokaż hasło</div>
         <div id='nr'>1</div>
         <div id='kat'>Kategoria</div>
         <div id="kategoria"><?=$kat?></div>
